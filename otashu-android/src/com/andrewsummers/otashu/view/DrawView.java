3eb9543fe1e99cb8fc784bfc7b65d5090240ad1f
datanets <Andrew.Summers@oit.edu>
2015-03-08 23:52:26 -0700

    emofing updates

--- otashu-android/src/com/andrewsummers/otashu/view/DrawView.java
+++ otashu-android/src/com/andrewsummers/otashu/view/DrawView.java
@@ -7,6 +7,12 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
+/**
+ * View a generated picture of a particular emotion's fingerprint.
+ * <p>
+ * This activity allows a user to see how an emotion's fingerprint appears.  
+ * </p>
+ */
 public class DrawView extends View {
     Paint paint = new Paint();
 
@@ -14,18 +20,6 @@ public class DrawView extends View {
         super(context);
     }
 
-    /**
-     * <pre>
-     * Example:
-     *   1 2 3 4 5 6 7 8 9 10 11 12
-     * 0 x   x   x   x
-     * 1   x   x x     x
-     * 2     x   x   x   x
-     * 3 x   x   x         x
-     * 4 x   x   x               x
-     * 5   x   x   x   x
-     * </pre>
-     */
     @Override
     public void onDraw(Canvas canvas) {
         // 7. Plot root number reductions (the emofing)
