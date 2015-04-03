e8273fed50d2e73c22cfdf4f37df7c1fc5631fd4
datanets <Andrew.Summers@oit.edu>
2015-04-03 00:15:31 -0700

    emofing updates

--- otashu-android/src/com/andrewsummers/otashu/activity/ViewEmotionFingerprintDetailActivity.java
+++ otashu-android/src/com/andrewsummers/otashu/activity/ViewEmotionFingerprintDetailActivity.java
@@ -1,11 +1,11 @@
 
 package com.andrewsummers.otashu.activity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.andrewsummers.otashu.data.EdgesDataSource;
 import com.andrewsummers.otashu.model.Edge;
-import com.andrewsummers.otashu.model.Note;
 import com.andrewsummers.otashu.view.DrawView;
 
 import android.app.Activity;
@@ -13,6 +13,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.util.SparseArray;
 
 /**
@@ -43,6 +44,7 @@ public class ViewEmotionFingerprintDetailActivity extends Activity {
         drawView.setBackgroundColor(Color.BLACK);
         setContentView(drawView);
 
+        // 1. Choose an emotion
         emotionId = (int) getIntent().getExtras().getLong("list_id");
 
         // get emotion graph id for Apprentice's note relationships graph
@@ -50,26 +52,76 @@ public class ViewEmotionFingerprintDetailActivity extends Activity {
         graphId = Long.parseLong(sharedPref.getString(
                 "pref_emotion_graph_for_apprentice", "1"));
 
-        gatherEmotionPaths(0.5f);
+        // 2. Gather all found paths
+        List<Integer> foundPathNotes = gatherEmotionPaths(0.5f);
+
+        // 6. Loop through all found paths
+        // TODO: add logic
+
+        // 7. Plot root number reductions (the emofing)
+        // TODO: add logic
+
     }
 
-    public SparseArray<List<Note>> gatherEmotionPaths(float maxWeight) {
-        SparseArray<List<Note>> foundPaths = new SparseArray<List<Note>>();
+    public List<Integer> gatherEmotionPaths(float maxWeight) {
+        // SparseArray<List<Note>> foundPaths = new SparseArray<List<Note>>();
+        List<Integer> foundPaths = new ArrayList<Integer>();
 
-        // TODO: add logic
-        // 2. Look for all possible Emotion Graph paths that are stronger than X weight
+        // 3. Look for all possible Emotion Graph paths that are stronger (lower than) X weight
         EdgesDataSource eds = new EdgesDataSource(this);
         SparseArray<List<Edge>> foundEdges = eds.getPathsForEmotion(graphId, emotionId, 0.5f);
 
-        // TODO: add logic
-        // 5. Convert noteset paths into root numbers +/- {1, ..., 12}
-        for (int i = 0; i < foundEdges.size(); i++) {
-            
+        // 5. Store noteset path root number reductions into data structure
+        SparseArray<Integer> rootNumbersMap = new SparseArray<Integer>();
+        boolean rootNumbersMapCreated = false;
+
+        Log.d("MYLOG", "edges: " + foundEdges.toString());
+
+        try {
+            for (int i = 1; i < foundEdges.size() + 1; i++) {
+                for (Edge edge : foundEdges.get(i)) {
+
+                    if (!rootNumbersMapCreated) {
+                        // firstNote is what is used for the root number start (note "I")
+                        int firstNote = 0;
+
+                        if (edge.getPosition() == 1) {
+                            firstNote = edge.getFromNodeId();
+                            rootNumbersMap.put(firstNote, 1);
+                            Log.d("MYLOG", "root numbers map... key: " + firstNote + " j: " + 1);
+                        }
+
+                        // 4. Convert noteset paths into root numbers +/- {1, ..., 12} (ex: I, II,
+                        // III, IV ...)
+                        // QUESTION: do this for each path?
+                        // range of notevalues between C4...B4 (60...71)
+
+                        int key = firstNote;
+                        for (int j = 2; j < 13; j++) {
+                            if (key > 70) {
+                                key = 60;
+                            } else {
+                                key++;
+                            }
+                            rootNumbersMap.put(key, j);
+                            Log.d("MYLOG", "root numbers map... key: " + key + " j: " + j);
+                        }
+                        rootNumbersMapCreated = true;
+                    }
+
+                    // add found mapped notevalues to our result
+                    foundPaths.add(rootNumbersMap.get(edge.getFromNodeId()));
+
+                    if (edge.getPosition() == 3) {
+                        foundPaths.add(rootNumbersMap.get(edge.getToNodeId()));
+                    }
+                }
+            }
+
+            Log.d("MYLOG", ">> found paths: " + foundPaths);
+        } catch (Exception e) {
+            Log.d("MYLOG", e.getStackTrace().toString());
         }
-
-        // TODO: add logic
-        // 6. Store noteset path root number reductions into data structure
-
         return foundPaths;
     }
 }
--- otashu-android/src/com/andrewsummers/otashu/view/DrawView.java
+++ otashu-android/src/com/andrewsummers/otashu/view/DrawView.java
@@ -24,12 +24,16 @@ public class DrawView extends View {
     public void onDraw(Canvas canvas) {
         // 7. Plot root number reductions (the emofing)
         paint.setColor(Color.BLUE);
+        paint.setAlpha(51);
         canvas.drawRect(0, 0, 50, 50, paint);
         paint.setColor(Color.RED);
+        paint.setAlpha(102);
         canvas.drawRect(50, 0, 100, 50, paint);
         paint.setColor(Color.YELLOW);
+        paint.setAlpha(153);
         canvas.drawRect(100, 0, 150, 50, paint);
         paint.setColor(Color.GREEN);
+        paint.setAlpha(204);
         canvas.drawRect(150, 0, 200, 50, paint);
     }
 }
