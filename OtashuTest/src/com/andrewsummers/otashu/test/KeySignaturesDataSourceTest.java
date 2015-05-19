a185e6d995e86d9b5b79de89c761c72c44d37fe4
datanets <Andrew.Summers@oit.edu>
2015-05-18 22:56:02 -0700

    testing updates

---
+++ OtashuTest/src/com/andrewsummers/otashu/test/KeySignaturesDataSourceTest.java
@@ -0,0 +1,78 @@
+
+package com.andrewsummers.otashu.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import com.andrewsummers.otashu.data.KeySignaturesDataSource;
+import com.andrewsummers.otashu.data.OtashuDatabaseHelper;
+import com.andrewsummers.otashu.model.KeySignature;
+
+public class KeySignaturesDataSourceTest extends AndroidTestCase {
+
+    private KeySignaturesDataSource ksds;
+    private OtashuDatabaseHelper db;
+    private RenamingDelegatingContext context;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        context = new RenamingDelegatingContext(getContext(), "test_");
+        db = new OtashuDatabaseHelper(context);
+        ksds = new KeySignaturesDataSource(context);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        db.close();
+        super.tearDown();
+    }
+
+    public void test_createKeySignature_paramKeySignature() throws Throwable {
+        KeySignature keySignature = new KeySignature();
+        keySignature.setApprenticeId(1);
+        keySignature.setEmotionId(1);
+        KeySignature result = ksds.createKeySignature(keySignature);
+        assertNotNull("created key signature is not null", result);
+    }
+
+    public void test_deleteKeySignature_paramKeySignature() throws Throwable {
+        KeySignature keySignature = new KeySignature();
+        keySignature.setId(1);
+        keySignature.setApprenticeId(1);
+        keySignature.setEmotionId(1);
+        KeySignature result = ksds.createKeySignature(keySignature);
+        assertNotNull("created key signature is not null", result);
+        ksds.deleteKeySignature(keySignature);
+        keySignature = new KeySignature();
+        keySignature = ksds.getKeySignature(1);
+        assertTrue(keySignature.getId() != 1);
+    }
+
+    public void test_getAllKeySignatures() throws Throwable {
+        test_createKeySignature_paramKeySignature();
+        List<KeySignature> keySignatures = new ArrayList<KeySignature>();
+        keySignatures = ksds.getAllKeySignatures(1);
+        assertNotNull("get all key signatures is not null", keySignatures);
+        assertFalse(keySignatures.isEmpty());
+    }
+
+    public void test_getKeySignature_paramKeySignatureId() throws Throwable {
+        test_createKeySignature_paramKeySignature();
+        KeySignature keySignature = ksds.getKeySignature(1);
+        assertNotNull("get key signature is not null", keySignature);
+    }
+
+    public void test_updateKeySignature_paramKeySignature() throws Throwable {
+        test_createKeySignature_paramKeySignature();
+        KeySignature keySignature = ksds.getKeySignature(1);
+        keySignature.setEmotionId(2);
+        ksds.updateKeySignature(keySignature);
+        KeySignature keySignature2 = ksds.getKeySignature(1);
+        assertNotNull("update key signature is not null", keySignature2);
+        assertTrue(keySignature.getEmotionId() == 2);
+    }
+}
