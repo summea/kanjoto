859c3a96234a8ebfdc9a6ffe50663ab686acfc8b
datanets <Andrew.Summers@oit.edu>
2015-05-21 23:16:58 -0700

    testing updates

---
+++ OtashuTest/src/com/andrewsummers/otashu/test/VerticesDataSourceTest.java
@@ -0,0 +1,78 @@
+
+package com.andrewsummers.otashu.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import com.andrewsummers.otashu.data.VerticesDataSource;
+import com.andrewsummers.otashu.data.OtashuDatabaseHelper;
+import com.andrewsummers.otashu.model.Vertex;
+
+public class VerticesDataSourceTest extends AndroidTestCase {
+
+    private VerticesDataSource vds;
+    private OtashuDatabaseHelper db;
+    private RenamingDelegatingContext context;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        context = new RenamingDelegatingContext(getContext(), "test_");
+        db = new OtashuDatabaseHelper(context);
+        vds = new VerticesDataSource(context);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        db.close();
+        super.tearDown();
+    }
+
+    public void test_createVertex_paramVertex() throws Throwable {
+        Vertex learningStyle = new Vertex();
+        learningStyle.setGraphId(1);
+        learningStyle.setNode(60);
+        Vertex result = vds.createVertex(learningStyle);
+        assertNotNull("created vertex is not null", result);
+    }
+
+    public void test_deleteVertex_paramVertex() throws Throwable {
+        Vertex learningStyle = new Vertex();
+        learningStyle.setId(1);
+        learningStyle.setGraphId(1);
+        learningStyle.setNode(60);
+        Vertex result = vds.createVertex(learningStyle);
+        assertNotNull("created vertex is not null", result);
+        vds.deleteVertex(learningStyle);
+        learningStyle = new Vertex();
+        learningStyle = vds.getVertex(1, 1);
+        assertTrue(learningStyle.getId() != 1);
+    }
+
+    public void test_getAllVertices() throws Throwable {
+        test_createVertex_paramVertex();
+        List<Vertex> learningStyles = new ArrayList<Vertex>();
+        learningStyles = vds.getAllVertices();
+        assertNotNull("get all vertices is not null", learningStyles);
+        assertFalse(learningStyles.isEmpty());
+    }
+
+    public void test_getVertex_paramVertexId() throws Throwable {
+        test_createVertex_paramVertex();
+        Vertex learningStyle = vds.getVertex(1, 1);
+        assertNotNull("get vertex is not null", learningStyle);
+    }
+
+    public void test_updateVertex_paramVertex() throws Throwable {
+        test_createVertex_paramVertex();
+        Vertex learningStyle = vds.getVertex(1, 1);
+        learningStyle.setNode(61);
+        vds.updateVertex(learningStyle);
+        Vertex learningStyle2 = vds.getVertex(1, 1);
+        assertNotNull("update vertex is not null", learningStyle2);
+        assertTrue(learningStyle.getNode() == 61);
+    }
+}
